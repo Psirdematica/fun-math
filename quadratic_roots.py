b0966ac964fb9f82b604c34f0879bcfa32329fc8
Psirdem <setsoafiasedemjoseph1219@gmail.com>
2023-04-11 12:46:16 +0000

    added the a function to solve the quadratic equation after determining the nature of root

--- quadratic_roots.py
+++ quadratic_roots.py
@@ -1,7 +1,10 @@
 # this script will determine the nature of the roots of a quadratic equation
-
 # ax^2 + bx + c
 # using the descriminant b^2 - 4ac
+
+import math as m
+import cmath
+
 
 def descriminant(a, b, c):
     des = (b**2) - (4*a*c)
@@ -13,6 +16,30 @@ def descriminant(a, b, c):
         return "real repeated roots"
 
 
-print(descriminant(1, -7, 10))
-print(descriminant(3, 2, -1))
-print(descriminant(3, 2, 1))
+def quadratic_solve(a, b, c):
+    # check the nature of the roots before solving
+    nature_of_root = descriminant(a, b, c)
+    # using the nature of roots to determine the function to use
+    if (nature_of_root == "real and distinct roots"):
+        print(nature_of_root)
+        soln1 = (-b + m.sqrt(((b**2) - 4*a*c)))/(2*a)
+        soln2 = (-b - m.sqrt(((b**2) - 4*a*c)))/(2*a)
+        return soln1, soln2
+    elif (nature_of_root == "real repeated roots"):
+        print(nature_of_root)
+        soln1 = soln2 = (-b/2*a)
+        return soln1, soln2
+    else:
+        print(nature_of_root)
+        soln1 = complex((-b/2*a), (m.sqrt(abs((b**2) - 4*a*c))/(2*a)))
+        soln2 = complex((-b/2*a), - (m.sqrt(abs((b**2) - 4*a*c))/(2*a)))
+        return soln1, soln2
+
+
+# print(descriminant(1, -7, 10))
+# print(descriminant(3, 2, -1))
+# print(descriminant(3, 2, 1))
+
+print(quadratic_solve(1, -7, 10))  # x^2 -7x + 10 = 0
+print(quadratic_solve(3, 2, -1))  # 3x^2 + 2x - 1 = 0
+print(quadratic_solve(3, 2, 1))  # 3x^2 + 2x + 1 = 0
